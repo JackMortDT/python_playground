b56f4dc83724d1094c140fe2fa642dd868f4a404
LuisSas <luis@makingdevs.com>
2018-12-02 12:43:33 -0600

    Adding basic age program

---
+++ basics/app.py
@@ -0,0 +1,8 @@
+__author__ = "LuisSas"
+
+def age_program():
+    user_age = input("Enter your age: ")
+    age_seconds = int(user_age) * 365 * 24 * 60 * 60
+    print("Your age in seconds is {}".format(age_seconds))
+
+age_program()
