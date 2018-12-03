0051f97107b5259521f5cc0d965044f28d9daee0
LuisSas <luis@makingdevs.com>
2018-12-02 22:11:41 -0600

    Post object and add some static properties

--- basics/terminal_blog/app.py
+++ basics/terminal_blog/app.py
@@ -1,12 +1,5 @@
-__author__ = "LuisSas"
+from models.post import Post
 
-import pymongo
+post = Post("Title", "Content", "Author")
 
-uri = "mongodb://127.0.0.1:27017"
-client = pymongo.MongoClient(uri)
-database = client['fullstack']
-collection = database['students']
-
-students = [student['mark'] for student in collection.find({}) if student['mark'] == 99]
-
-print(students)
+print(post.title)
